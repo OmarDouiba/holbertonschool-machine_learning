f64e634377d84f4fd492fc8bda567d2a9fde0386
DouibaOmar <douibaomar40@gmail.com>
2025-05-22 15:19:31 +0100

    Done

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+Write a class Neuron that defines a single neuron performing binary classification:
+
+class constructor: def init(self, nx):
+   nx is the number of input features to the neuron
+       If nx is not an integer, raise a TypeError with the exception: nx must be an integer
+       If nx is less than 1, raise a ValueError with the exception: nx must be a positive integer
+   All exceptions should be raised in the order listed above
+Public instance attributes:
+   W: The weights vector for the neuron. Upon instantiation, it should be initialized using a random normal distribution.
+   b: The bias for the neuron. Upon instantiation, it should be initialized to 0.
+   A: The activated output of the neuron (prediction). Upon instantiation, it should be initialized to 0.
+alexa@ubuntu-xenial:$ cat 0-main.py
+#!/usr/bin/env python3
+
+import numpy as np
+
+Neuron = __import__('0-neuron').Neuron
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+print(neuron.W)
+print(neuron.W.shape)
+print(neuron.b)
+print(neuron.A)
+neuron.A = 10
+print(neuron.A)
+alexa@ubuntu-xenial:$ ./0-main.py
+[[ 1.76405235e+00  4.00157208e-01  9.78737984e-01  2.24089320e+00
+   1.86755799e+00 -9.77277880e-01  9.50088418e-01 -1.51357208e-01
+
+...
+
+  -5.85865511e-02 -3.17543094e-01 -1.63242330e+00 -6.71341546e-02
+   1.48935596e+00  5.21303748e-01  6.11927193e-01 -1.34149673e+00]]
+(1, 784)
+0
+0
+10
+alexa@ubuntu-xenial:$
+"""
+import numpy as np
+
+class Neuron:
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
